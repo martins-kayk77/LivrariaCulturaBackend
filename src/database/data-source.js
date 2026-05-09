a6cf14bceced065fb631bed3fe61471041de5982
Kaike Martins <kaikesilvamartins10.com>
2026-05-09 18:21:55 -0300

    ajuste data-source

--- src/database/data-source.js
+++ src/database/data-source.js
@@ -1,18 +1,23 @@
 import "dotenv/config";
 import "reflect-metadata";
-import {DataSource} from "typeorm"
+import { DataSource } from "typeorm";
+
+const useSSL = process.env.DB_SSL === "true";
 
 const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    port: Number(process.env.DB_PORT),
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: ["src/entities/*.js"],
-    migrations: ["src/database/migrations/*.cjs"]
-
+  type: "mysql",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  port: Number(process.env.DB_PORT),
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: useSSL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
+  entities: ["src/entities/*.js"],
+  migrations: ["src/database/migrations/*.cjs"],
 });
 
-export {AppDataSource}
-
+export { AppDataSource };
